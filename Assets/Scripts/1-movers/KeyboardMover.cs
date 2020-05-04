d5e2cfe0637a5e5d9498520a8736883e28b1a4d9
YitshakS <aa@bb.cc>
2020-05-05 01:33:13 +0300

    Initial Commit

---
+++ Assets/Scripts/1-movers/KeyboardMover.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * This component moves its object when the player clicks the arrow keys.
+ */
+public class KeyboardMover: MonoBehaviour {
+    [Tooltip("Speed of movement, in meters per second")]
+    [SerializeField] float speed = 1f;
+
+    void Update() {
+        float horizontal = Input.GetAxis("Horizontal"); // +1 if right arrow is pushed, -1 if left arrow is pushed, 0 otherwise
+        float vertical = Input.GetAxis("Vertical");     // +1 if up arrow is pushed, -1 if down arrow is pushed, 0 otherwise
+        Vector3 movementVector = new Vector3(horizontal, vertical, 0) * speed * Time.deltaTime;
+        transform.position += movementVector;
+
+        //get the screen position
+        Vector3 scrPos = Camera.main.WorldToScreenPoint(transform.position);
+        //Check if we are too far left
+        if (scrPos.x < 0) TeleportRight(scrPos);
+        //check if we are too far right
+        if (scrPos.x > Screen.width) TeleportLeft(scrPos);
+
+
+        // אם הגענו לגבול נעביר את החללית לצד השני
+        //if (transform.position.x >= Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, transform.position.y)).x)
+        //    Debug.Log("right");
+        //transform.position = Camera.main.ScreenToWorldPoint(new Vector2(0, transform.position.y));
+        //transform.Translate(movementVector);
+        // NOTE: "Translate(movementVector)" uses relative coordinates - 
+        //       it moves the object in the coordinate system of the object itself.
+        // In contrast, "transform.position += movementVector" would use absolute coordinates -
+        //       it moves the object in the coordinate system of the world.
+        // It makes a difference only if the object is rotated.
+    }
+    
+    // אם השחקן מתנגש בקיר
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Boom");
+        if (other.tag == "Wall")
+            transform.position = new Vector2(transform.position.x - Input.GetAxis("Horizontal"), transform.position.y - Input.GetAxis("Vertical"));
+    }
+
+    //TeleportRight moves character from it's current x position to x position 10 pixels left from the right edge of the screen   
+    void TeleportRight(Vector3 scrPos)
+    {
+
+        //this is the position on the screen we want to move the character too, we only want to change it's x-coordinate
+        Vector3 goalScrPos = new Vector3(Screen.width - 10, scrPos.y, scrPos.z);
+
+        //translate goal screen position to world position
+        Vector3 targetWorldPos = Camera.main.ScreenToWorldPoint(goalScrPos);
+
+        //move player
+        transform.position = targetWorldPos;
+
+    }
+
+    //TeleportRight moves character from it's current x position to x position 10 pixels left from the right edge of the screen   
+    void TeleportLeft(Vector3 scrPos)
+    {
+
+        //this is the position on the screen we want to move the character too, we only want to change it's x-coordinate
+        Vector3 goalScrPos = new Vector3(0 + 10, scrPos.y, scrPos.z);
+
+        //translate goal screen position to world position
+        Vector3 targetWorldPos = Camera.main.ScreenToWorldPoint(goalScrPos);
+
+        //move player
+        transform.position = targetWorldPos;
+
+    }
+}
---
+++ Assets/Scripts/3-collisions/ShieldThePlayer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldThePlayer : MonoBehaviour {
+    [Tooltip("The number of seconds that the shield remains active")] [SerializeField] float duration;
+    [Tooltip("")]
+    [SerializeField]
+    float minTimeRandom;
+
+    [SerializeField]
+    float maxTimeRandom;
+
+    [SerializeField]
+    float maxTimeShow;
+
+    [SerializeField]
+    GameObject shield; // המגן שמופיע על השחקן
+
+    void Start()
+    {
+        InvokeRepeating("ChangePosition", 0, Random.Range(minTimeRandom, maxTimeRandom)); // calls ChangePosition() every ... secs
+        shield.SetActive(false); // כיבוי המגן
+    }
+
+    void ChangePosition()
+    {
+        if ((int)Random.Range(0, maxTimeShow) == 0)
+        {
+            float spawnY = Random.Range
+                (Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y, Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y);
+            float spawnX = Random.Range
+                (Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x);
+
+            Vector2 spawnPosition = new Vector2(spawnX, spawnY);
+            transform.position = spawnPosition;
+        }
+        else
+            transform.position = new Vector2(-10, -10);
+    }
+
+    // בודק האם משהו התנגש במגן
+    private void OnTriggerEnter2D(Collider2D other) {
+         if (other.tag == "Player") { // אם המשהו הזה הוא השחקן
+            Debug.Log("Shield triggered by player");
+            var destroyComponent = other.GetComponent<DestroyOnTrigger2D>();
+            other.GetComponent<Animation>().Play(); // הפעלת האנימציה של העיגול סביב השחקן
+            if (destroyComponent) {
+                destroyComponent.StartCoroutine(ShieldTemporarily(destroyComponent));
+                // NOTE: If you just call "StartCoroutine", then it will not work, 
+                //       since the present object is destroyed!
+                Destroy(gameObject);  // Destroy the shield itself - prevent double-use
+            }
+        } else {
+            Debug.Log("Shield triggered by "+other.name);
+        }
+    }
+    private IEnumerator ShieldTemporarily(DestroyOnTrigger2D destroyComponent) {
+        shield.SetActive(true);
+        destroyComponent.enabled = false;
+        for (float i = duration; i > 0; i--) {
+            Debug.Log("Shield: " + i + " seconds remaining!");
+            yield return new WaitForSeconds(1);
+        }
+        Debug.Log("Shield gone!");
+        shield.SetActive(false);
+        destroyComponent.enabled = true;
+    }
+}
